d29fa60f137a0151387f47ae34a5cd6653e2f0a0
Nemer EL-Sahli <nemer.sahli@gmx.de>
2019-05-12 00:36:13 +0200

    testing expo with browser and device

--- hello_world2/App.js
+++ hello_world2/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 export default class App extends React.Component {
+  _onPress = () => {
+    alert('hi');
+  };
   render() {
     return (
       <View style={styles.container}>
         <Text>Thank you for using React Native expo.</Text>
-
         <Image
+          onPress={this._onPress}
           source={{ uri: 'https://facebook.github.io/react/logo-og.png' }}
           style={{ width: 100, height: 100, marginTop: 20 }}
         />
+
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPress}
+            title="Hello"
+            color="black"
+            accessibilityLabel="Tap on Me"
+          />
+        </View>
       </View>
     );
   }
@@ -22,5 +34,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20
+  },
+  buttonContainer: {
+    marginTop: 10,
+    borderRadius: 10
   }
 });
