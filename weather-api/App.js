b4e0f33ab483587c787feccf7e8bab2cb380e19d
Nemer EL-Sahli <nemer.sahli@gmx.de>
2019-06-16 23:58:50 +0200

    weather api modified

--- weather-api/App.js
+++ weather-api/App.js
@@ -1,33 +1,46 @@
 import React, { Component } from 'react';
-import { View, Image, Text, ImageBackground } from 'react-native';
+import { View, Image, Text,ImageBackground ,TextInput} from 'react-native';
 
 export default class WeatherApi extends Component {
+  constructor(props){
+    super(props);
+    this.state = { city: '' };
+  }
   render() {
     return (
       <View>
-        <ImageBackground
-          source={{
-            uri:
-              'https://images.pexels.com/photos/1743398/pexels-photo-1743398.jpeg?cs=srgb&dl=4k-wallpaper-calm-environment-1743398.jpg&fm=jpg'
+
+      <ImageBackground source={{uri:'https://images.pexels.com/photos/1743398/pexels-photo-1743398.jpeg?cs=srgb&dl=4k-wallpaper-calm-environment-1743398.jpg&fm=jpg'}} style={{width: '100%', height: '100%'}}>
+
+        <Text style={{ fontSize: 50,
+          marginTop:30,
+         textAlign: 'center',
+         color:'white'
+          }}>Weather API</Text>
+        <Text
+          style={{
+            backgroundColor: 'transparent',
+            textAlign: 'center',
+            fontSize: 30,
+           
+            padding: 40,
+       
           }}
-          style={{ width: '100%', height: '100%' }}
         >
-          <Text style={{ fontSize: 50, textAlign: 'center', color: 'white' }}>
-            Weather API
-          </Text>
-          <Text
-            style={{
-              backgroundColor: 'transparent',
-              textAlign: 'center',
-              fontSize: 30,
-              color: 'white',
-              padding: 40,
-              flex: 1
-            }}
-          >
-            some text will be here!
-          </Text>
-        </ImageBackground>
+          Enter a City Name
+        </Text>
+             <TextInput
+          style={{ height: 40 , color:'black', backgroundColor:'rgba(255,255,255,.5)', textAlign:'center'}}
+          placeholder="City Name..."
+          onChangeText={text => this.setState({ city:text })}
+        />
+           <Text style={{ fontSize: 20,
+          marginTop:30,
+         textAlign: 'center',
+         color:'white'
+          }}>{this.state.city}</Text>
+      </ImageBackground>
+
       </View>
     );
   }
